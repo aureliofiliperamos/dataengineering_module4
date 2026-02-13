f5d202298a2980eaf3e808a9e097e9a9688a979c
Filipe Ramos <filipe.ramos@zohomail.eu>
2026-02-13 20:31:00 +0000

    Update of staging models and addition of intermediate models

--- models/staging/localbike/stg_localbike__orders.sql
+++ models/staging/localbike/stg_localbike__orders.sql
@@ -1,9 +1,9 @@
 SELECT order_id,
     customer_id,
     order_status,
-    SAFE_CAST(order_date AS DATE) AS created_order_at,
-    SAFE_CAST(required_date AS DATE) AS required_order_at,
-    SAFE_CAST(shipped_date AS DATE) AS shipped_date_at,
+    SAFE_CAST(order_date AS DATE) AS order_date,
+    SAFE_CAST(required_date AS DATE) AS required_date,
+    SAFE_CAST(shipped_date AS DATE) AS shipped_date,
     store_id,
     staff_id
 FROM {{ source('localbike', 'orders') }}
--- models/staging/localbike/stg_localbike__stores.sql
+++ models/staging/localbike/stg_localbike__stores.sql
@@ -1,9 +1,9 @@
 SELECT store_id,
     UPPER(store_name) AS store_name,
-    phone,
+    phone AS phone_number,
     email,
-    UPPER(street) AS address,
+    UPPER(street) AS street_address,
     UPPER(city) AS city,
     UPPER(state) AS state,
-    zip_code
+    zip_code AS postal_code
 FROM {{ source('localbike', 'stores') }}
